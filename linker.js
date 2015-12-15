ed983b5a3f1d3fd02527ca33e1b9c9c5f03da290
Bill McMillin <billmcmillin@gmail.com>
2015-12-15 12:24:57 -0500

    remove mouseover

--- linker.js
+++ linker.js
@@ -5,33 +5,23 @@ jQuery(function($){
         if(pageInitialized) return;
         pageInitialized = true;
         $("document").ready(function() {
+
+        /*container styles*/
+        /*$(".contentindex").html("<span>Article Linker</span>");
+        $(".contentindex").css({"font-style":"italic", "font-size": "1.2em","margin-left":"60px"});
+        */
         /*sidebar styles*/
-            $(".hide-side").css("visibility","hidden");
-            $(".hide-side2").css("visibility","hidden");
-            $("a:contains('Search the library catalog')").addClass("btn searchCat-action");
+           // $(".hide-side").css("visibility","hidden");
+           // $(".hide-side2").css("visibility","hidden");
+            $("a:contains('Search the catalog for print version')").addClass("btn searchCat-action");
             $("a:contains('Request via Interlibrary Loan')").addClass("btn ill-action");
-            $("<h3 class=\"custom-links-header\">Not available online or through the catalog?</h3>").insertBefore(".xill");
-            $("<h3>Questions?</h3>").insertBefore(".ask_us");
+            $("<h3 class=\"custom-links-header\">2. Not available at UC?</h3>").insertBefore(".xill");
+            $("<h3>3. Need help?</h3>").insertBefore(".ask_us");
             $(".ask_us a").addClass("btn ask-action");
             $(".ask-action").css("margin-bottom", "10px");
 
-
-           
-
         /*sidebar actions*/   
-            $(".sidebar-title").on("mousemove", onSideMouseOver);
-            $(".sidebar-title").on("mouseleave", onSideMouseLeave);
-            $(".xill").on("mousemove", onSide2MouseOver);
-            $(".xill").on("mouseleave", onSide2MouseLeave);
-
-
-            /*non-sidebar links*/
-
-                $("#col2_button").on("mousemove", onMouseOver);
-                $("#col2_button").on("mouseleave", onMouseLeave);
-
-                $("#col1_button").on("mousemove", onCatMouseOver);
-                $("#col1_button").on("mouseleave", onCatMouseLeave);
+      
                 //get the current URL
                 var itemURL = window.location.href;
                 //base URLs for different ILL forms
@@ -42,20 +32,33 @@ jQuery(function($){
 
             /*for catalog searches*/
                 //grab the title from the URL
-                var title = itemURL.replace(/.*&title=/i, "");
-                var title = title.replace(/&.*/i, "");
+                if(itemURL.indexOf('title') != -1){
+                    var title = itemURL.replace(/.*title=/i, "");
+                    var title = title.replace(/&.*/i, "");
+                }
+                else{
+                    var title = "NULL";
+                }
+                
 
                 //grab issn from the URL
-                var issn = itemURL.replace(/.*issn=/i, "");
-                var issn = issn.replace(/&.*/i, "");
+                if(itemURL.indexOf('issn') != -1){
+                    var issn = itemURL.replace(/.*issn=/i, "");
+                    var issn = issn.replace(/&.*/i, "");
+                }
+                else{
+                    var issn = "NULL";
+                }
+                
 
                 //append it to the base URL
                 var search1 = base3 + title;
                 var search2 = base4 + issn;
 
                 //update the link in html
-                $("#by_title").html("<a href=\x22" + search1 + "\x22>By Title</a>");
-                $("#by_issn").html("<a href=\x22" + search2 + "\x22>By ISSN</a>");
+                $(".searchCat-action").html("<a href=\x22" + search1 + "\x22 target=\"_blank\" style=\"color:#333\">Search the catalog for print version</a>");
+                $("#by_title").html("<a href=\x22" + search1 + "\x22 target=\"_blank\">By Title</a>");
+                $("#by_issn").html("<a href=\x22" + search2 + "\x22 target=\"_blank\">By ISSN</a>");
 
                 //now update the CSS
                 $("#by_title").css({"display":"block","margin-left":"10px", "margin-top":"5px"});
@@ -70,47 +73,15 @@ jQuery(function($){
                 var open2 = base2 + elements;
 
                 //update the link in html
-                $("#illiad_link").html("<a href=\x22" + open1 + "\x22>Interlibrary Loan Form</a>");
-                $("#hsl_link").html("<a href=\x22" + open2 + "\x22>Health Sciences Library Interlibrary Loan Form</a>");
-
+                $(".ill-action").html("<a href=\x22" + open1 + "\x22 target=\"_blank\" style=\"color:#333\">Request via Interlibrary Loan</a>");
+                $("#illiad_link").html("<a href=\x22" + open1 + "\x22target=\"_blank\">Interlibrary Loan Form</a>");
+                $("#hsl_link").html("<a href=\x22" + open2 + "\x22target=\"_blank\">Health Sciences Library Interlibrary Loan Form</a>");
                 //now update the CSS
                 $("#illiad_link").css({"display":"block","margin-left":"10px", "margin-top":"5px"});
                 $("#hsl_link").css({"display":"block","margin-left":"10px", "margin-top":"5px"});
 
             });
             
-
-            function onMouseOver(evt) {
-                $("#col2_content").css("visibility", "visible");   
-            }
-            
-            function onMouseLeave(evt) {
-                $("#col2_content").css("visibility", "hidden");
-            }
-
-            function onCatMouseOver(evt) {
-                $("#col1_content").css("visibility", "visible");
-            }
-            
-            function onCatMouseLeave(evt) {
-                $("#col1_content").css("visibility", "hidden");
-            }
-
-            function onSideMouseOver(evt) {
-                $(".hide-side").css("visibility", "visible");
-            }
-            
-            function onSideMouseLeave(evt) {
-                $(".hide-side").css("visibility", "hidden");
-            }
-
-            function onSide2MouseOver(evt) {
-                $(".hide-side2").css("visibility", "visible");
-            }
-            
-            function onSide2MouseLeave(evt) {
-                $(".hide-side2").css("visibility", "hidden");
-            }
     });
 
 
